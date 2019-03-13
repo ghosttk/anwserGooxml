581ad4a59464dc039d959f8008fa22f9013612f3
ghosttk <ghosttk@hotmail.com>
2019-03-13 17:13:00 +0800

    finishing add write file functionality

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
-    "regexp"
-    "io/ioutil"
+	"os"
+	"regexp"
 
 	"baliance.com/gooxml/document"
 )
@@ -15,50 +16,57 @@ func main() {
 		log.Fatalf("error opening document: %s", err)
 	}
 
-    regexpNumber:= regexp.MustCompile(`\d\.$`)
-    regexpAnwser:= regexp.MustCompile(`【答案】`)
-    regexpJieXi:= regexp.MustCompile(`【解析】`)
+	regexpNumber := regexp.MustCompile(`\d\.$`)
+	regexpAnwser := regexp.MustCompile(`【答案】`)
+	regexpJieXi := regexp.MustCompile(`【解析】`)
 
 	paragraphs := []document.Paragraph{}
 	for _, p := range doc.Paragraphs() {
 		paragraphs = append(paragraphs, p)
 	}
 
+	var anwserSlice []string
+    strAnwser := ""
 	for _, p := range paragraphs {
-            /*
-		for _, r := range p.Runs() {
-                m := regexpNumber.MatchString(r.Text())
-                if (m) {
-                        fmt.Println(r.Text())
-                        //n := regexpAnswer.MatchString(
-                }
+		/*
+					for _, r := range p.Runs() {
+			                m := regexpNumber.MatchString(r.Text())
+			                if (m) {
+			                        fmt.Println(r.Text())
+			                        //n := regexpAnswer.MatchString(
+			                }
+					}
+		*/
+		rs := p.Runs()
+		i := 0
+		bstart := false
+		for i = 0; i < len(rs); i++ {
+			r := rs[i]
+			m := regexpNumber.MatchString(r.Text())
+			if m {
+				rn := rs[i+1]
+				n := regexpAnwser.MatchString(rn.Text())
+				if n {
+					bstart = true
+				}
+			}
+			bjieXi := regexpJieXi.MatchString(r.Text())
+			if bjieXi {
+				bstart = false
+                strAnwser += "\n"
+			}
+			if bstart {
+				anwserSlice = append(anwserSlice, r.Text())
+				strAnwser += r.Text()
+			}
 		}
-            */
-            rs := p.Runs()
-            i := 0
-            bstart := false
-            var anwserSlice []string
-            for i=0; i<len(rs); i++ {
-                r := rs[i]
-                m := regexpNumber.MatchString(r.Text())
-                if (m) {
-                    rn := rs[i+1]
-                    n := regexpAnwser.MatchString(rn.Text())
-                    if (n) {
-                            bstart = true
-                    }
-                }
-                bjieXi := regexpJieXi.MatchString(r.Text())
-                if (bjieXi) {
-                        bstart = false
-                }
-                if (bstart) {
-                        data := []byte(r.Text())
-                        err := ioutil.WriteFile("anwser.txt", data, 0644)
-                        anwserSlice = append(anwserSlice, r.Text())
-                } 
-            }
-            fmt.Println(anwserSlice)
+		//		fmt.Println(anwserSlice)
+	}
+	fmt.Println(strAnwser)
+	data := []byte(strAnwser)
+	err = ioutil.WriteFile("anwser.txt", data, os.ModeAppend)
+	if err != nil {
+		panic(err)
 	}
 
 	doc.SaveToFile("edit-document.docx")
